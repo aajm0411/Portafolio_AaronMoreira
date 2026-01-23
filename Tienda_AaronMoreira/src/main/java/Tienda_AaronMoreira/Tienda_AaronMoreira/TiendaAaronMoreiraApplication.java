52e44616ae6c492b1acfdb2e82ffee412712714e
aajm0411 <ajmg200705@gmail.com>
2026-01-23 10:12:57 -0600

    Semana2-AaronMoreira

---
+++ Tienda_AaronMoreira/src/main/java/Tienda_AaronMoreira/Tienda_AaronMoreira/TiendaAaronMoreiraApplication.java
@@ -0,0 +1,13 @@
+package Tienda_AaronMoreira.Tienda_AaronMoreira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAaronMoreiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAaronMoreiraApplication.class, args);
+	}
+
+}
---
+++ Tienda_AaronMoreira/src/test/java/Tienda_AaronMoreira/Tienda_AaronMoreira/TiendaAaronMoreiraApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_AaronMoreira.Tienda_AaronMoreira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAaronMoreiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
