24acee2666c3e64cbbbca44ae63b76f2a5817ffc
aajm0411 <ajmg200705@gmail.com>
2026-03-25 19:01:05 -0600

    Semana10

---
+++ Tienda_AaronMoreira/src/main/java/Tienda_AaronMoreira/repository/RutaRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda_AaronMoreira.repository;
+
+import Tienda_AaronMoreira.domain.Ruta;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RutaRepository extends JpaRepository<Ruta, Integer> {
+    
+    public List<Ruta> findAllByOrderByRequiereRolAsc();
+    
+}
